330ede17c92fd524574cdba4ce373e65697d2f6e
grantrisk <riskgrant@gmail.com>
2022-04-03 22:13:14 -0400

    Setup basic api calls

--- src/blueprints/index.py
+++ src/blueprints/index.py
@@ -1,4 +1,9 @@
+import flask
+import requests
+import json
 from flask import Blueprint, render_template
+
+from static.resources.keys import IEX_CLOUD_API_TOKEN
 
 index_page = Blueprint("index", __name__, static_folder="static", template_folder="templates")
 
@@ -9,4 +14,12 @@ def index():
     This method returns the index page.
     :return: render_template('index.html')
     """
-    return render_template('index.html')
+
+    symbol = 'AAPL'
+    api_url = f'https://sandbox.iexapis.com/stable/stock/{symbol}/quote?token={IEX_CLOUD_API_TOKEN}'
+    # api_url = f'https://cloud.iexapis.com/stable/account/metadata?token={IEX_CLOUD_API_TOKEN}'
+    data = requests.get(api_url).json()
+
+
+    return flask.jsonify(**data)
+    # return render_template('index.html', data=data)
